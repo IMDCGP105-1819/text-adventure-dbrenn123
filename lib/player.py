7309a468c97782e708b152ca79b81640df179ded
Daniel <d.brennan@uos.ac.uk>
2018-12-16 21:20:00 +0000

    Add location property to Player class

--- lib/player.py
+++ lib/player.py
@@ -1,6 +1,28 @@
+from lib.world import World
+
 class Player:
 	def __init__(self):
-		return
+		self._location = None
 
 	def __repr__(self):
 		return f"<{self.__class__.__name__}()>"
+
+	@property
+	def location(self):
+		'''Get the current location of the player
+
+		Returns <Room()>
+		'''
+
+		return self._location
+
+	@location.setter
+	def location(self, room_id):
+		'''Set the current location of the player
+
+		Params
+			room_id : int
+				ID used to load room object from data file.
+		'''
+
+		self._location = World.load_room(room_id)
--- test/u_player.py
+++ test/u_player.py
@@ -1,6 +1,17 @@
+import mock
 from lib.player import Player
 
 class TestClass:
 	def t_repr(self):
+		assert repr(Player()) == "<Player()>"
+
+class TestSetLocation:
+	from lib.room import Room
+	from lib.world import World
+
+	@mock.patch('lib.world.World.load_room', return_value=Room("Test"))
+	def t_0(self, load_room):
 		p = Player()
-		assert repr(p) == "<Player()>"
+		p.location = load_room()
+
+		assert p.location.name == "Test"
