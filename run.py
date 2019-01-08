ecfa042ea340f71a6cae4805cb95e9936780f2f3
Daniel <d.brennan@uos.ac.uk>
2019-01-08 23:49:53 +0000

    Add quit and reset commands

--- run.py
+++ run.py
@@ -1,20 +1,39 @@
+""" Game cycle.
+
+Parse player input, execute command and print response.
+
+TODO:
+	- More helpful InvalidCommandError messages
+"""
+
 import re
 
 import lib.command as command
 import lib.player as player
 
-player.init(0)
+def start():
+	player.init(0)
+
+start()
 
 while(True):
-	input = input(">")
+	CMD = input(">")
 
-	try:
-		print(re.sub("\t", "", command.parse(input)()))
-	except command.MissingActionError:
-		print("##Missing action")
-	except command.TargetActionMismatchError:
-		print("##That item cannot do that")
-	except command.InvalidCommandError:
-		print("##Invalid command")
-	except Exception as e:
-		raise e
+	if(CMD.upper() == "QUIT"):
+		break
+	elif(CMD.upper() == "RESET"):
+		start()
+	else:
+		try:
+			callback = command.parse(CMD)
+			response = callback()
+
+			print(re.sub("\t", "", response))
+		except command.MissingActionError:
+			print("##Missing action")
+		except command.TargetActionMismatchError:
+			print("##That item cannot do that")
+		except command.InvalidCommandError:
+			print("##Invalid command")
+		except Exception as e:
+			raise e
