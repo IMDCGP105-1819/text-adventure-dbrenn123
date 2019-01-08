4bae0212f18be3327d0e49e43510c619d59c2b86
Daniel <d.brennan@uos.ac.uk>
2019-01-08 22:37:20 +0000

    Rename in game objects to items to differentiate them

--- lib/stage.py
+++ lib/stage.py
@@ -1,47 +1,54 @@
 from lib.game_object import GameObject
 
 class Stage(GameObject):
-	"""Object representing an area for the player to explore
+	""" A class representing an area for the player to explore.
 
-		Params:
-			name: string - Name of area
-			description: string - Description of area
-			objects: [] - List of objects contained which can be interacted with
+	Parameters
+		name (string):
+			Name of stage.
+
+		description (string):
+			Description of stage.
+
+		items (list[ lib.game_item.GameItem ]):
+			List of items which can be interacted with.
 	"""
 
-	def __init__(self, name, description, objects):
+	def __init__(self, name, description, items):
 		super().__init__(name, description)
 
-		self._objects = objects
-
-	def __repr__(self):
-		return f"<{self.__class__.__name__}({self._name})>"
+		self._items = items
 
 	@property
-	def objects_list(self):
-		"""Get list of names of each object within stage
+	def items_list(self):
+		""" Get a list of the names of each item within the stage.
 
-			Returns string[]
+		Return (list[ string ]):
+			A list of item names.
 		"""
-		return [obj.name for obj in self._objects]
+		return [item.name for item in self._items]
 
-	def get_object(self, name):
-		"""Get instance of specified object
+	def get_item(self, name):
+		""" Get item by name.
 
-			Params:
-				name: string - Name of required object
+		Parameters
+			name (string):
+				Name of required item.
 
-			Returns first object to match name parameter, False if there is no
-			match.
+		Return (lib.game_item.GameItem):
+			First item to match name parameter or False if there is no match.
 		"""
-		for obj in self._objects:
-			if(obj.name.upper() == name.upper()):
-				return obj
+
+		for item in self._items:
+			if(item.name.upper() == name.upper()):
+				return item
 
 		return False
 
 	def examine(self):
+		""" Print stage info and interactable item names. """
+
 		print(f"You look around the {self.name}.")
 		super().examine()
-		print(f"You see the following objects...")
-		print("\n".join([str(obj) for obj in self.objects_list]))
+		print(f"You see the following items...")
+		print("\n".join([str(item) for item in self.items_list]))
