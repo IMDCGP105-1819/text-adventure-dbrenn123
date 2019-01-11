909f97853005c958f8f300fe6b4b9aff6b22acef
Daniel <d.brennan@uos.ac.uk>
2019-01-11 05:51:07 +0000

    Add function for writing to game data file

--- lib/world.py
+++ lib/world.py
@@ -1,34 +1,64 @@
-""" This module provides an interface for importing game locations from data file. """
+""" This module provides an interface for importing and writing game content from data file. """
 
-from xml.etree import ElementTree
+from xml.etree.ElementTree import ElementTree
 
 from lib.stage import Stage
-from lib.game_item import GameItem, Door
+from lib.game_item import *
 
-def _get_document():
-	""" Get root element of game data XML.
+_DATA_PATH = 'res/world.xml'
+_TREE = ElementTree()
+_TREE.parse(_DATA_PATH)
 
-	Return (xml.etree.ElementTree)
+def _collect_items(stage_element):
+	""" Get list of items in stage
+
+	Parameters
+		stage_element (xml.etree.ElementTree)
+
+	Return (list[ lib.game_item.GameItem ])
 	"""
 
-	try:
-		return ElementTree.parse('res/world.xml').getroot()
-	except Exception as e:
-		raise e
+	items = []
 
-def load_stage(id):
-	""" Load stage data by ID
+	for item in stage_element.findall("content/*"):
+		Item = None
 
-	Paremeters
-		id (integer):
-			ID used to read stage info from data file.
+		# Assign item class based on tag name.
+		if item.tag == "freedom":
+			Item = Freedom
+		elif item.tag == "lamp":
+			Item = Lamp
+		else:
+			Item = GameItem
 
-	Return (lib.stage.Stage)
+		items.append(Item(*_get_attribs(item, 'name', 'description')))
+
+	for door in stage_element.findall("join/door"):
+		items.append(Door(*_get_attribs(door, 'id', 'description', 'dest_id')))
+
+	return items
+
+def _collect_joins(stage_element):
+	""" Get directional stage joins
+
+	Parameters
+		stage_element (xml.etree.ElementTree)
+
+	Return (dict{ direction: dict{name:str, id:int } })
 	"""
 
-	stage_element = _get_document().find(f"stage[@id='{id}']")
+	joins = {}
 
-	return Stage(*_get_attribs(stage_element, 'name', 'description'), _collect_items(stage_element), _collect_joins(stage_element))
+	for join in stage_element.findall("join/*"):
+		tag = join.tag
+
+		if tag != "door":
+			dest_id = int(_get_attribs(join, 'dest_id')[0])
+			dest_name = _get_attribs(_TREE.find(f"stage[@id='{dest_id}']"), 'name')[0]
+
+			joins[tag] = {'id': dest_id, 'name': dest_name}
+
+	return joins
 
 def _get_attribs(element, *attrib_keys):
 	""" Get attribute values from element data
@@ -50,37 +80,30 @@ def _get_attribs(element, *attrib_keys):
 
 	return attrib_vals
 
-def _collect_items(stage_element):
-	""" Get list of items in stage
+def load_stage(id):
+	""" Load stage data by ID
 
-	Return (list[ lib.game_item.GameItem ])
+	Paremeters
+		id (integer):
+			ID used to read stage info from data file.
+
+	Return (lib.stage.Stage)
 	"""
 
-	items = []
+	stage_element = _TREE.find(f"stage[@id='{id}']")
 
-	for item in stage_element.findall("content/*"):
-		items.append(GameItem(*_get_attribs(item, 'name', 'description')))
+	return Stage(*_get_attribs(stage_element, 'name', 'description'), _collect_items(stage_element), _collect_joins(stage_element))
 
-	for door in stage_element.findall("join/door"):
-		items.append(Door(*_get_attribs(door, 'description', 'dest_id')))
+def write(xpath, attrib, new_val):
+	""" Write new data to document
 
-	return items
-
-def _collect_joins(stage_element):
-	""" Get directional stage joins
-
-	Return (dict{ direction: dict{name:str, id:int } })
+	TODO:
+		-Better API for altering game object data.
 	"""
 
-	joins = {}
+	elem = _TREE.find(xpath)
+	old_val = elem.attrib[attrib]
 
-	for join in stage_element.findall("join/*"):
-		tag = join.tag
-
-		if tag != "door":
-			dest_id = int(_get_attribs(join, 'dest_id')[0])
-			dest_name = _get_attribs(_get_document().find(f"stage[@id='{dest_id}']"), 'name')[0]
-
-			joins[tag] = {'id': dest_id, 'name': dest_name}
-
-	return joins
+	if new_val != old_val:
+		elem.set(attrib, new_val)
+		_TREE.write(_DATA_PATH)
