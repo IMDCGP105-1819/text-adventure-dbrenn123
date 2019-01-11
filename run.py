15012b8d661309640cb2fdd9c675e26e45a2738a
Daniel <d.brennan@uos.ac.uk>
2019-01-11 06:12:44 +0000

    Copy game data file from template on init

--- run.py
+++ run.py
@@ -7,20 +7,24 @@ TODO:
 """
 
 import re
+from shutil import copyfile
 
 import lib.command as command
 import lib.player as player
+from lib.game_item import Freedom as run
 
 class _InvalidResponseError(Exception):
 	""" Raise when response from callback is incorrect type """
 	pass
 
-def start():
+def init():
+	copyfile("res/init_world.xml", "res/world.xml")
 	player.init(0)
+	print("Use the command (look around) to begin...")
 
-start()
+init()
 
-while(True):
+while(run):
 	CMD = input(">")
 
 	if(CMD.upper() == "QUIT"):
@@ -35,15 +39,27 @@ while(True):
 			print('\n' + re.sub("\t", "", response))
 		except command.MissingActionError:
 			print("[!] Missing action.")
+
+		except command.MissingTargetError:
+			print("[!] Missing target.")
+
 		except command.ActionConflictError:
 			print("[!] Too many actions.")
+
+		except command.InvalidTargetError:
+			print("[!] Item cannot be found.")
+
 		except command.InvalidDirectionError:
 			print("[!] There is nothing that way.")
+
 		except command.TargetActionMismatchError:
 			print("[!] You cannot do that with that item.")
+
 		except command.InvalidCommandError:
 			print("[!] Invalid command")
+
 		except TypeError:
 			raise _InvalidResponseError(f"Invalid response from {callback}. Expected type string. Got type {type(response)}")
+			
 		except Exception as e:
 			raise e
